22debbdd705d9cec17ce7190c71e38f29690f9cf
Firestar311 <firestar311mc@gmail.com>
2021-06-19 22:15:54 -0500

    Added some things to the channel

--- src/main/java/com/starmediadev/starmcutils/chat/channel/Channel.java
+++ src/main/java/com/starmediadev/starmcutils/chat/channel/Channel.java
@@ -1,8 +1,47 @@
 package com.starmediadev.starmcutils.chat.channel;
 
-import com.starmediadev.starmcutils.chat.format.ChatFormat;
+import org.bukkit.command.CommandSender;
 
 public class Channel {
-    protected String name, permission;
-    protected ChatFormat format;
+    protected static final String VAR_PREFIX = "{channelprefix}", VAR_SENDER = "{sender}", VAR_MESSAGE = "{chatmessage}"; 
+    
+    protected String name, permission, format, prefix, playerNameFormat, messageFormat, baseColor;
+    
+    public Channel(String name) {
+        this.name = name;
+    }
+    
+    public String format(CommandSender sender, String message) {
+        String output = format;
+        output = output.replace(VAR_MESSAGE, messageFormat.replace("{message}", message));
+        return output;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getPlayerNameFormat() {
+        return playerNameFormat;
+    }
+
+    public String getMessageFormat() {
+        return messageFormat;
+    }
+
+    public String getBaseColor() {
+        return baseColor;
+    }
 }
