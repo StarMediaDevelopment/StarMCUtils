8ed7c70485dc5d9afcbf90887a73be8b9ea41973
Firestar311 <slaterandrew0398@gmail.com>
2021-07-31 17:32:06 -0500

    Added a soft data dependency to StarMCUtils for StarData as well as an option integration for the Postion as a type handler as the Position class is in this library

--- src/main/java/com/starmediadev/plugins/starmcutils/StarMCUtils.java
+++ src/main/java/com/starmediadev/plugins/starmcutils/StarMCUtils.java
@@ -1,9 +1,11 @@
 package com.starmediadev.plugins.starmcutils;
 
+import com.starmediadev.plugins.starmcutils.integrations.StarDataIntegration;
 import com.starmediadev.plugins.starmcutils.region.SelectionManager;
 import com.starmediadev.plugins.starmcutils.updater.Updater;
 import com.starmediadev.plugins.starmcutils.util.ColorUtils;
 import com.starmediadev.plugins.starmcutils.util.MCUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -13,13 +15,16 @@ public class StarMCUtils extends JavaPlugin implements Listener {
     
     private SelectionManager selectionManager = new SelectionManager();
     private Updater updater;
+    private StarDataIntegration starDataIntegration;
 
     public void onEnable() {
         updater = new Updater(this);
         getServer().getScheduler().runTaskTimer(this, updater, 1L, 1L);
         getServer().getPluginManager().registerEvents(this, this);
 
-        ColorUtils.addCustomColor("&g", "#aa2c66");
+        if (Bukkit.getPluginManager().getPlugin("StarData") != null) {
+            this.starDataIntegration = new StarDataIntegration(this);
+        }
     }
     
     @EventHandler
---
+++ src/main/java/com/starmediadev/plugins/starmcutils/integrations/PositionTypeHandler.java
@@ -0,0 +1,31 @@
+package com.starmediadev.plugins.starmcutils.integrations;
+
+import com.starmediadev.data.handlers.DataTypeHandler;
+import com.starmediadev.data.model.DataType;
+import com.starmediadev.plugins.starmcutils.util.Position;
+
+public class PositionTypeHandler extends DataTypeHandler<Position> {
+    public PositionTypeHandler() {
+        super(Position.class, DataType.VARCHAR, 1000);
+    }
+
+    public Object serializeSql(Object object) {
+        if (object instanceof Position position) {
+            return position.getX() + "," + position.getY() + "," + position.getZ() + "," + position.getYaw() + "," + position.getPitch();
+        }
+        return null;
+    }
+
+    public Position deserialize(Object object) {
+        if (object instanceof String str) {
+            String[] split = str.split(",");
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
+            int z = Integer.parseInt(split[2]);
+            float yaw = Float.parseFloat(split[3]);
+            float pitch = Float.parseFloat(split[4]);
+            return new Position(x, y, z, yaw, pitch);
+        }
+        return null;
+    }
+}
