c83de916e1dc89cf980f851ffc83c4c383d86175
Firestar311 <slaterandrew0398@gmail.com>
2022-02-12 05:04:10 -0600

    Added some world based utilities to StarMCUtils

--- src/main/java/com/starmediadev/plugins/starmcutils/util/TickUtils.java
+++ src/main/java/com/starmediadev/plugins/starmcutils/util/TickUtils.java
@@ -2,6 +2,12 @@ package com.starmediadev.plugins.starmcutils.util;
 
 public final class TickUtils {
     private TickUtils(){}
+    
+    public static final String TIME_FORMAT = "h:mm a";
+    
+    public static long asMilliseconds(long duration) {
+        return duration / 50;
+    }
 
     public static int asSeconds(int duration) {
         return duration * 20;
