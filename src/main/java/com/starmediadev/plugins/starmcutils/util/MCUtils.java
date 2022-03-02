adb659c4b5ac18afd5020c030e02dff87eb9ae4f
Firestar311 <slaterandrew0398@gmail.com>
2022-03-02 14:42:05 -0600

    Added a method to filter different types of color translation based on boolean parameters

--- src/main/java/com/starmediadev/plugins/starmcutils/util/MCUtils.java
+++ src/main/java/com/starmediadev/plugins/starmcutils/util/MCUtils.java
@@ -54,29 +54,40 @@ public final class MCUtils {
      * @return The colored text
      */
     public static String color(String uncolored) {
-        String text = ChatColor.translateAlternateColorCodes('&', uncolored);
+        return color(uncolored, true, true, true);
+    }
+    
+    public static String color(String uncolored, boolean translateBukkit, boolean translateCustom, boolean translateHex) {
+        String text = uncolored;
+        if (translateBukkit) {
+            text = ChatColor.translateAlternateColorCodes('&', uncolored);
+        }
         StringBuilder colored = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
             if (ColorUtils.validColorChar(c)) {
-                if (text.length() >= i + 2) {
-                    String code = String.valueOf(c) + text.charAt(i + 1);
-                    ChatColor color = ColorUtils.getCustomColor(code);
-                    colored.append(color);
-                    i++;
+                if (translateCustom) {
+                    if (text.length() >= i + 2) {
+                        String code = String.valueOf(c) + text.charAt(i + 1);
+                        ChatColor color = ColorUtils.getCustomColor(code);
+                        colored.append(color);
+                        i++;
+                    }
                 }
             } else if (c == '#') {
-                if (text.length() > i + 6) {
-                    String colorCode = c + text.substring(i + 1, i + 7);
-                    ChatColor color = ChatColor.of(colorCode);
-                    colored.append(color);
-                    i += 6;
+                if (translateHex) {
+                    if (text.length() > i + 6) {
+                        String colorCode = c + text.substring(i + 1, i + 7);
+                        ChatColor color = ChatColor.of(colorCode);
+                        colored.append(color);
+                        i += 6;
+                    }
                 }
             } else {
                 colored.append(c);
             }
         }
-        
+    
         return colored.toString();
     }
     
